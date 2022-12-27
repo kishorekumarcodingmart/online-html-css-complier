0703d29a306b07a4c1afc505bc23acb3b7c8d58a
kishorekumarcodingmart <kishorekumar@codingmart.com>
2022-12-27 21:02:46 +0530

    CodeWorking

--- src/Components/CodeEditor/CodeEditor.jsx
+++ src/Components/CodeEditor/CodeEditor.jsx
@@ -1,44 +1,21 @@
-import Editor from '@monaco-editor/react'
-import { useSelector, useDispatch } from 'react-redux'
-import {useCurrentCode} from '../../Hook/useCurrentCode'
-import {addHtmlCode, addCssCode, addJavascriptCode} from '../../Redux/code'
+import { useSelector } from 'react-redux'
 import React from 'react'
 import CodeEditorNavbar from '../CodeEditorNavbar/CodeEditorNavbar'
 import './CodeEditor.css'
+import Html from './Html'
+import Css from './Css'
+import Javascript from './Javascript'
 
 function CodeEditor() {
 
-  const dispatch = useDispatch()
-
   const currentLanguage = useSelector((state)=>state.language.value)
-
-  const updateCode = (code) => {
-    if (currentLanguage === "html"){
-      dispatch(addHtmlCode(code))
-    }
-    else if (currentLanguage === "css"){
-      dispatch(addCssCode(code))
-    }
-    else {
-      dispatch(addJavascriptCode(code))
-    }
-  }
 
   return (
     <div className='codeWrapper'>
       <CodeEditorNavbar />
-      <Editor 
-        value={useCurrentCode()}
-        height="87vh"
-        theme='vs-dark'
-        language={currentLanguage}
-        options={{
-          scrollBeyondLastLine:false,
-          fontSize:"16px"
-        }}
-        onChange={(code)=> updateCode(code)}
-
-      />
+      {
+        (currentLanguage==="html")?<Html/>:(currentLanguage==="css")?<Css />:<Javascript />
+      }
     </div>
   )
 }
--- src/Components/Output/Output.jsx
+++ src/Components/Output/Output.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
+import { useSelector } from 'react-redux'
+import './Output.css'
 
 function Output() {
+
+  const [srcDoc, setSrcDoc] = useState('')
+
+  const sandbox = [
+    'allow-scripts',
+    'allow-forms',
+    'allow-modals',
+    'allow-orientation-lock',
+    'allow-pointer-lock',
+    'allow-popups',
+    'allow-popups-to-escape-sandbox',
+    'allow-presentation',
+    'allow-same-origin',
+    'allow-scripts',
+    'allow-top-navigation',
+    'allow-top-navigation-by-user-activation'
+  ]
+
+  const html = useSelector((state)=>state.code.html)
+  const css = useSelector((state)=>state.code.css)
+  const js = useSelector((state)=>state.code.javascript)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
   return (
-    <div className='output'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, assumenda dolor? Veniam temporibus esse facilis eius, necessitatibus rem harum doloremque voluptate inventore perferendis magni dignissimos nisi saepe accusamus consectetur veritatis?</div>
+    <div className='output'>
+      <iframe 
+        srcDoc={srcDoc}
+        className='iframeOutput' 
+        title="output" 
+        sandbox={sandbox.join(" ")}
+        ></iframe>
+    </div>
   )
 }
 
